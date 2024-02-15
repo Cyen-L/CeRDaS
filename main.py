e169941c6604d008eac9487cf2be8cd402e332dd
Cy <nyen_1994@hotmail.com>
2024-02-15 18:35:34 +0800

    Fix

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ class IrisFeatures(BaseModel):
     petal_length: float
     petal_width: float
 
-@app.post("/predict/")
+class IrisFeaturesList(BaseModel):
+    iris_features: List[IrisFeatures]
+
+@app.post("/predict_single/")
 async def predict_species(iris: IrisFeatures):
     features = [[iris.sepal_length, iris.sepal_width, iris.petal_length, iris.petal_width]]
     prediction = model.predict(features)
     print(prediction)
     return {"predicted_species": prediction[0]}
+
+
+@app.post("/predict_multi/")
+async def predict_species(iris_list: List[IrisFeatures]):
+    features = [[iris.sepal_length, iris.sepal_width, iris.petal_length, iris.petal_width] for iris in iris_list]
+    predictions = model.predict(features)
+    return {"predicted_species": predictions.tolist()}
