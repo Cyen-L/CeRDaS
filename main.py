07f0286d23b279d6340c18955e274bb51d472b66
Cy <nyen_1994@hotmail.com>
2024-02-16 15:00:18 +0800

    algo_train_api_update

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from typing import List, Union
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 # Load trained model
 model = joblib.load('iris_model.pkl')
@@ -19,15 +21,27 @@ class IrisFeatures(BaseModel):
     petal_width: float
 
 class IrisFeaturesList(BaseModel):
+    model_name: str = None
     iris_features: List[IrisFeatures]
 
 class BaseTrainRequest(BaseModel):
     saved_name: str = None
-    random_state: int = None
 
 class RandomForestTrainRequest(BaseTrainRequest):
     max_depth: int = None
     n_estimators: int = 100
+    random_state: int = None
+
+class LogisticRegressionTrainRequest(BaseTrainRequest):
+    penalty: str = 'l2'
+    dual: bool = False
+    tol: float = 1e-4
+    C: float = 1.0
+    fit_intercept: bool = True
+    intercept_scaling: float = 1.0
+    random_state: int = None
+    solver: str = 'lbfgs'
+
 
 @app.on_event("startup")
 async def startup_event():
@@ -58,17 +72,48 @@ async def predict_species(iris_data: Union[IrisFeatures, IrisFeaturesList]):
         features = [[iris_val.sepal_length, iris_val.sepal_width, iris_val.petal_length, iris_val.petal_width] for iris_val in iris_data.iris_features]
     else:
         return {'Fromat Error'}
+    if iris_data.model_name:
+        model_path = os.path.join('models', iris_data.model_name + '.pkl')
+        if not os.path.exists(model_path):
+            return {"Message": "Model not exist", "Model_Path": str(model_path)}
+    else:
+        model_path = 'iris_model.pkl'
+    joblib.load(model_path)
     predictions = model.predict(features)
-    return {"predicted_species": predictions.tolist()}
+    return {"Message": "Model Prediction Successful", "Model_Path":model_path, "Predicted_Species": predictions.tolist()}
 
 @app.post("/train/random_forest/")
 def train_random_forest(request: RandomForestTrainRequest):
-    saved_path = os.path.join('models', request.saved_name+'.pkl')
+    saved_path = os.path.join('models', request.saved_name + '.pkl')
     if os.path.exists(saved_path):
-        return {"message": "Model saved name exist"}
+        return {"Message": "Model saved name exist"}
     else:
         # Train a Random Forest Classifier1
         model = RandomForestClassifier(random_state=request.random_state, max_depth=request.max_depth, n_estimators=request.n_estimators)
         model.fit(X_train, y_train)
         joblib.dump(model, saved_path)
-        return {"message": "Model trained successfully"}
+        return {"Message": "Model Training Successful", "Model":"Random Forest Classifier", "Saved_Path": saved_path}
+
+@app.post("/train/gaussian_naive_bayes/")
+def train_gaussian_naive_bayes(request: BaseTrainRequest):
+    saved_path = os.path.join('models', request.saved_name + '.pkl')
+    if os.path.exists(saved_path):
+        return {"Message": "Model saved name exist"}
+    else:
+        # Train a Random Forest Classifier1
+        model = GaussianNB()
+        model.fit(X_train, y_train)
+        joblib.dump(model, saved_path)
+        return {"Message": "Model Training Successful", "Model": "Gaussian Naive Bayes", "Saved_Path": saved_path}
+
+@app.post("/train/logistic_regression/")
+def train_logistic_regression(request: LogisticRegressionTrainRequest):
+    saved_path = os.path.join('models', request.saved_name + '.pkl')
+    if os.path.exists(saved_path):
+        return {"Message": "Model saved name exist"}
+    else:
+        # Train a Random Forest Classifier1
+        model = LogisticRegression(penalty=request.penalty, dual=request.dual, tol=request.tol, C=request.C, fit_intercept=request.fit_intercept, intercept_scaling=request.intercept_scaling, random_state=request.random_state, solver=request.solver)
+        model.fit(X_train, y_train)
+        joblib.dump(model, saved_path)
+        return {"Message": "Model Training Successful", "Model": "Logistic Regression", "Saved_Path": saved_path}
